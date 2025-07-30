b1e1d1076396cdf92af8a5f358f57844c97e55dc
Anusha Ratra <anusha.ratra@gmail.com>
2025-07-29 23:49:41 -0400

    Fixed name

--- src/App.jsx
+++ src/App.jsx
@@ -345,7 +345,7 @@ ${formData.message}
       {/* Footer */}
       <footer className="footer">
         <div className="container">
-          <p>&copy; 2024 Your Name. All rights reserved.</p>
+          <p>&copy; 2024 Anusha Ratra. All rights reserved.</p>
         </div>
       </footer>
     </div>
