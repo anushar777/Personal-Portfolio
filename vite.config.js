bd93717373df64fe1ee92b0c6745479220140a33
Anusha Ratra <anusha.ratra@gmail.com>
2025-07-30 22:07:43 -0400

    Fix GitHub Pages base path to /Personal-Portfolio/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolioWeb/',
+  base: '/Personal-Portfolio/',
   build: {
     outDir: 'docs',
     emptyOutDir: true
